aa13ae6ae2f35da4b5dd7a21200b5598bce62950
Michael Nipper <mjn4406@gmail.com>
2013-01-24 22:15:56 -0500

    Add sort by title and release date

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,12 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params[:sort_by].present?
+      @movies = Movie.order("title ASC") if params[:sort_by] == 'title'
+      @movies = Movie.order("release_date ASC") if params[:sort_by] == 'release_date'
+    else
+      @movies = Movie.all
+    end
   end
 
   def new
